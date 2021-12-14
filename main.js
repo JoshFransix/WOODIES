c370596f999be228d33741a396d48a7b53c22731
joshfransix <joshiandersonk69@gmail.com>
2021-12-14 08:54:40 +0100

    final_Modifications(home, responsive)

--- main.js
+++ main.js
@@ -1,40 +1,68 @@
 const hamburger = document.querySelector(".hamburger");
-const lineOne = hamburger.querySelector(".line-one");
-const lineTwo = hamburger.querySelector(".line-two");
-const lineThree = hamburger.querySelector(".line-three");
 
-const lines = [lineOne, lineTwo, lineThree];
+const body = document.querySelector("body");
 
-var tl = gsap.timeline();
+const navMenu = document.querySelector(".nav-links");
 
-var toggleMenu = gsap.timeline({ paused: true });
-toggleMenu.reversed(true); // IMPORTANT to set the init state to true
+const navLinks = document.querySelectorAll(".nav-item");
 
-const nextStateMap = {
-  true: () => {
-    toggleMenu.play();
-    toggleMenu.reversed(false);
-  },
-  false: () => {
-    toggleMenu.reverse();
-    toggleMenu.reversed(true);
-  },
-};
+const header = document.querySelector("#header");
 
-toggleMenu
-  .to(lineTwo, 0.25, { transformOrigin: "50% 50%", scale: 0 }, 0)
-  .to(lineThree, 0.25, { transformOrigin: "50% 50%" }, "change")
-  .to(lineOne, 0.25, { transformOrigin: "50% 50%" }, "change")
-  .to(lineThree, 0.25, { y: -15 }, 0)
-  .to(lineOne, 0.25, { y: 15 }, 0)
-  .to(lineThree, 0.25, { rotation: -45 }, "cross")
-  .to(lineOne, 0.25, { rotation: 45 }, "cross");
+hamburger.addEventListener("click", mobileMenu);
 
-hamburger.addEventListener("click", () => {
-  const isReversed = toggleMenu.reversed();
-  hamburger.classList.toggle("js-x");
-  nextStateMap[isReversed]();
+function mobileMenu() {
+  hamburger.classList.toggle("hamburger-open");
+  body.classList.toggle("lock-scroll");
+  navMenu.classList.toggle("open");
+
+  navLinks.forEach((navLinks) => {
+    navLinks.classList.toggle("fade");
+  });
+  // navLinks.classList.add("active");
+}
+
+navLinks.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("hamburger-open");
+  body.classList.remove("lock-scroll");
+  navMenu.classList.remove("open");
+  navLinks.forEach((navLink) => {
+    navLink.classList.remove("fade");
+  });
+}
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
+// const lines = [lineOne, lineTwo, lineThree];
+
+// var tl = gsap.timeline();
+
+// var toggleMenu = gsap.timeline({ paused: true });
+// toggleMenu.reversed(true); // IMPORTANT to set the init state to true
+
+// const nextStateMap = {
+//   true: () => {
+//     toggleMenu.play();
+//     toggleMenu.reversed(false);
+//   },
+//   false: () => {
+//     toggleMenu.reverse();
+//     toggleMenu.reversed(true);
+//   },
+// };
+
+// toggleMenu
+//   .to(lineTwo, 0.25, { transformOrigin: "50% 50%", scale: 0 }, 0)
+//   .to(lineThree, 0.25, { transformOrigin: "50% 50%" }, "change")
+//   .to(lineOne, 0.25, { transformOrigin: "50% 50%" }, "change")
+//   .to(lineThree, 0.25, { y: -15 }, 0)
+//   .to(lineOne, 0.25, { y: 15 }, 0)
+//   .to(lineThree, 0.25, { rotation: -45 }, "cross")
+//   .to(lineOne, 0.25, { rotation: 45 }, "cross");
+
+// navLinks.forEach((n) => n.addEventListener("click", closeMenu));
 
 // // Create li element
 // lis = document.createElement("li");
